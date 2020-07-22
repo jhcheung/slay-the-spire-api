93722f99a141781fda0850b740939b7b60edebc3
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-07-22 17:28:56 -0400

    added rack-cors

--- config/application.rb
+++ config/application.rb
@@ -35,3 +35,13 @@ module SlayTheSpireApi
     config.api_only = true
   end
 end
+
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+      :headers => :any,
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :max_age => 0
+  end
+end
