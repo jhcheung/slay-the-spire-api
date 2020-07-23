6d4ba943b091a2f32aa1f22442f7e2eff50c8a11
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-07-22 21:19:42 -0400

    added routes, set controllers

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::CardsController < ApplicationController
+
+    def index
+        card = Card.all
+        render json: CardSerializer.new(card)
+    end
+
+
+    def show
+        card = Card.find(params[:id])
+        render json: CardSerializer.new(card)
+    end
 end
--- app/controllers/api/v1/relics_controller.rb
+++ app/controllers/api/v1/relics_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::RelicsController < ApplicationController
+
+    def index
+        relic = Relic.all
+        render json: RelicSerializer.new(relic)
+    end
+
+    def show
+        relic = Relic.find(params[:id])
+        render json: RelicSerializer.new(relic)
+    end
 end
