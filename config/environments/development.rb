04c1a81eb9aaff6eaf1851b3d387cc8833ec4268
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2020-07-29 03:19:09 -0700

    avoid N+1 query by eager loading
    also added logging during development
    
    For more details on N+1:
    
    - https://stackoverflow.com/questions/57357892/rails-reducing-the-amount-of-activestorage-queries
    - https://api.rubyonrails.org/classes/ActiveStorage/Attached/Model.html#method-i-has_one_attached

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,13 +42,20 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.log_level = :debug
+  config.log_tags = [ :request_id ]
+
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # see https://stackoverflow.com/a/38239345/198348
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
 
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
