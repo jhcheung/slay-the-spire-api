92fefac1dc0a85b8be9120dbf716f32c1783b143
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-07-28 23:17:29 -0400

    added cors info

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,15 @@ module SlayTheSpireApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end    
   end
 end
